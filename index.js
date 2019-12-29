091e996639867e03f637ea06285283d8cdf9ad5c
macw007 <derekelum@gmail.com>
2019-12-29 10:14:40 -0600

    intial test location

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+var url =require('url');
+export.handler = function(event,context,callback){
+	
+	var list = ['www.google.com',''];
+	for(var i=0;i<list.length;i++){
+		var target =list[i];
+		
+		var urlObject =url.parse(target);
+		
+		var mod = require(urlObject.substring(0,urlObject.length-1));
+    
+		var req = mod.request(urlObject,function(res){
+			
+			res.setEncoding('utf8');
+			res.on('data',function(chunk){
+				//console.log
+				
+			});			
+						
+			res.on('end',function(){
+				//console.log
+				callback();
+			});
+			
+						
+		});
+		
+				
+		req.on('error',function(e){			
+			//console.log
+			callback(e);			
+			
+		});
+		
+		req.end();						
+	}	
+};
