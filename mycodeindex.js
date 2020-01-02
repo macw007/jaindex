1a2e2638f00bff66b9c9e5cfbf883bbb4ec7e190
macw007 <derekelum@gmail.com>
2020-01-01 18:13:26 -0600

    inital commit

---
+++ mycodeindex.js
@@ -0,0 +1,37 @@
+'use strict'
+
+var url =require('url');
+
+function handler(){	
+	
+	let list = ['https://www.google.com/','https://www.yahoo.com/'];
+	for(let i=0;i<list.length; i++){
+		
+		setTimeout( function(){ 		
+		var target =list[i];
+    const https = require('https');
+
+    https.get(target, (resp) => {
+    let data = '';
+
+  // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+  // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      console.log("data was successfully collected"+data);
+    });
+
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+		
+       }, 5000);
+		
+	}	
+	
+}
+handler();
+
